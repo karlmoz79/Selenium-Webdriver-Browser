46a160c86d3199cb71704e9178cc0faa575c2857
karlmoz79 <karlmoz@hotmail.com>
2026-02-19 19:59:06 -0500

    chore: actualización automática desde workflow

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import search
 from selenium import webdriver  # Importa el módulo principal de Selenium que permite controlar el navegador
 from selenium.webdriver.common.by import By  # Importa la clase By para estrategias de localización
 
@@ -24,19 +25,24 @@ driver.add_cookie({"name": "i18n-prefs", "value": "USD", "domain": ".amazon.com"
 driver.add_cookie({"name": "session-currency", "value": "USD", "domain": ".amazon.com"})
 
 # Le indica al navegador que navegue a la URL especificada mediante una petición HTTP GET.
-driver.get("https://www.amazon.com/Lenovo-ThinkBook-IRL-21SH000GUS-Computer/dp/B0F83ZRQM7/?currency=USD")
+#driver.get("https://www.amazon.com/Lenovo-ThinkBook-IRL-21SH000GUS-Computer/dp/B0F83ZRQM7/?currency=USD")
+driver.get("https://www.python.org/")
+
 
 # Busca el primer elemento en el HTML de la página (El DOM) que tenga exactamente la clase CSS "a-offscreen".
 # En la estructura de Amazon, esta clase suele aplicarse a elementos que contienen el precio oculto para lectores de pantalla.
-element = driver.find_element(By.CLASS_NAME, "a-offscreen")
+#element = driver.find_element(By.CLASS_NAME, "a-offscreen")
 
 # 1. Obtiene el texto interno completo del elemento, inclusive si está oculto en la pantalla (textContent)
 # 2. Usa el método de cadena .strip("$") para remover el símbolo de dólar de los extremos del texto.
 # 3. Guarda el resultado final (un String que parece número) en la variable price_float
-price_float = element.get_attribute("textContent").strip("$")
+#price_float = element.get_attribute("textContent").strip("$")
 
 # Imprime el valor recolectado y limpio en la terminal
-print(price_float)
+#print(price_float)
+
+search_bar = driver.find_element(By.NAME, value="q")
+print(search_bar.tag_name)
 
 # Cierra por completo el navegador y termina la sesión de WebDriver, liberando los recursos de memoria manejados por el "driver"
 driver.quit()
